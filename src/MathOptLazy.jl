a320edb7412165325fd39e90bc9c706e8fb283d6
Oscar Dowson <odow@users.noreply.github.com>
2026-09-22 12:20:58 +1200

    Fix MOI.ListOfConstraintAttributesSet (#19)

--- src/MathOptLazy.jl
+++ src/MathOptLazy.jl
@@ -491,6 +491,13 @@ function MOI.get(
     return ret
 end
 
+function MOI.get(
+    ::Optimizer,
+    ::MOI.ListOfConstraintAttributesSet{F,LazyScalarSet{S}},
+) where {F<:MOI.AbstractScalarFunction,S<:MOI.AbstractScalarSet}
+    return MOI.AbstractConstraintAttribute[]
+end
+
 function MOI.supports(
     model::Optimizer,
     ::MOI.AbstractConstraintAttribute,
